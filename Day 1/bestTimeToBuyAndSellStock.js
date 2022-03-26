91f9147e8019d639fafb194376378b4ef506fb9e
Musadiq <musadiq.peerzada@smartserv.io>
2022-03-26 13:44:01 +0530

    added Day 1

---
+++ Day 1/bestTimeToBuyAndSellStock.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+ var maxProfit = function(prices) {
+    if(prices.length === 1) {
+        return 0;
+    }
+    let maxProfit = 0;
+    let buyDay = 0;
+    for(let i = 1; i < prices.length; i++) {
+        if(prices[buyDay] > prices[i]) {
+            buyDay = i
+        }
+        else {
+            maxProfit = Math.max(maxProfit, prices[i] - prices[buyDay]);
+        }
+    }
+    return maxProfit;  
+};
---
+++ Day 1/plusOne.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+ var plusOne = function(digits) {
+    return (BigInt(digits.join(""))+BigInt(1)).toString().split("")
+//     let len = digits.length - 1;
+//     res = digits;
+//     if (digits[len] === 9) {
+        
+//     }
+//     else {
+//         res[len] = digits[len] + 1
+//     }
+}
